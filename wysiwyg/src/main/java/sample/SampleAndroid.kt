3e382433fa2df6cce2732306e4dc48c1c0b95493
Saket Narayan <saket@saket.me>
2019-08-10 20:30:28 -0400

    Use androidx

--- wysiwyg/src/main/java/sample/SampleAndroid.kt
+++ wysiwyg/src/main/java/sample/SampleAndroid.kt
@@ -1,16 +1,16 @@
 package sample
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
 
-        val textView = findViewById<TextView>(R.id.main_text)
-        textView.text = Greeting().hello()
-    }
+    val textView = findViewById<TextView>(R.id.main_text)
+    textView.text = Greeting().hello()
+  }
 }
