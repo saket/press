efe0e8788f81705597e81f367eb44f6ebfe129b8
Saket Narayan <saket@saket.me>
2019-08-10 21:49:42 -0400

    Add ContourLayout

--- compose/src/main/java/home/HomeActivity.kt
+++ compose/src/main/java/home/HomeActivity.kt
@@ -1,16 +1,12 @@
 package home
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import me.saket.wysiwyg.Greeting
 
 class HomeActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(TextView(this).apply {
-      text = Greeting().hello()
-    })
+    setContentView(HomeView(this))
   }
 }
---
+++ compose/src/main/java/home/HomeView.kt
@@ -0,0 +1,12 @@
+package home
+
+import android.content.Context
+import androidx.appcompat.widget.Toolbar
+import com.squareup.contour.ContourLayout
+
+class HomeView(context: Context) : ContourLayout(context) {
+
+  private val toolbar = Toolbar(context).apply {
+
+  }
+}
