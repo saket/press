89522c3911baa3b84d99635c97261c9f254aed94
Saket Narayan <saket@saket.me>
2019-09-08 00:08:07 -0400

    Mark AppModule as AssistedModule

--- androidApp/src/main/java/compose/di/AppModule.kt
+++ androidApp/src/main/java/compose/di/AppModule.kt
@@ -1,11 +1,14 @@
 package compose.di
 
+import com.squareup.inject.assisted.dagger2.AssistedModule
 import compose.editor.EditorModule
 import compose.home.HomeModule
 import compose.theme.ThemeModule
 import dagger.Module
 
+@AssistedModule
 @Module(includes = [
+  AssistedInject_AppModule::class,
   ThemeModule::class,
   HomeModule::class,
   EditorModule::class
--- androidApp/src/main/java/compose/home/HomeModule.kt
+++ androidApp/src/main/java/compose/home/HomeModule.kt
@@ -1,14 +1,12 @@
 package compose.home
 
-import com.squareup.inject.assisted.dagger2.AssistedModule
 import dagger.Module
 import dagger.Provides
 import me.saket.compose.shared.home.HomePresenter
 import me.saket.compose.shared.home.SharedHomeComponent
 import me.saket.compose.shared.navigation.Navigator
 
-@AssistedModule
-@Module(includes = [AssistedInject_HomeModule::class])
+@Module
 object HomeModule {
 
   @Provides
