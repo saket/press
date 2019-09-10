e75fd5cc08f1003912c95c391d685925c9d63c57
Saket Narayan <saket@saket.me>
2019-09-09 23:26:52 -0400

    Smaller heading levels

--- wysiwyg/src/commonMain/kotlin/me/saket/wysiwyg/parser/node/HeadingLevel.kt
+++ wysiwyg/src/commonMain/kotlin/me/saket/wysiwyg/parser/node/HeadingLevel.kt
@@ -4,10 +4,10 @@ package me.saket.wysiwyg.parser.node
  * @param textSizeRatio Taken from HTML5 spec: [http://zuga.net/articles/html-heading-elements/
  */
 enum class HeadingLevel(val textSizeRatio: Float) {
-  H1(2f),
-  H2(1.5f),
-  H3(1.17f),
+  H1(1.40f),
+  H2(1.25f),
+  H3(1.10f),
   H4(1f),
-  H5(.83f),
-  H6(.75f);
+  H5(1f),
+  H6(1f);
 }
