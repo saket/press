64fc2f3d08e2db18ca0b664dfa4c931244ca99ac
Saket Narayan <saket@saket.me>
2019-09-15 06:01:59 -0400

    Use ISO8601 format for serializing DateTime

--- shared/src/commonMain/kotlin/me/saket/compose/shared/db/DateTimeAdapter.kt
+++ shared/src/commonMain/kotlin/me/saket/compose/shared/db/DateTimeAdapter.kt
@@ -1,12 +1,18 @@
 package me.saket.compose.shared.db
 
+import com.soywiz.klock.DateFormat
 import com.soywiz.klock.DateTime
+import com.soywiz.klock.parse
 import com.squareup.sqldelight.ColumnAdapter
 
 /**
  * For UTC date times.
  */
 class DateTimeAdapter : ColumnAdapter<DateTime, String> {
-  override fun decode(databaseValue: String): DateTime = DateTime.fromString(databaseValue).utc
-  override fun encode(value: DateTime) = value.toString()
+
+  override fun decode(databaseValue: String): DateTime =
+    DateFormat.FORMAT1.parse(databaseValue).utc
+
+  override fun encode(value: DateTime) =
+    value.toString(DateFormat.FORMAT1)
 }
