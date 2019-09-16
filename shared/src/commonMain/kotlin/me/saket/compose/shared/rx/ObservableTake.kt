76ab17585a7def506cf2eeef5d84cfe464762a5e
Saket Narayan <saket@saket.me>
2019-09-15 20:20:22 -0400

    Update ObservableTake with AtomicInt
    https://github.com/badoo/Reaktive/pull/193

--- shared/src/commonMain/kotlin/me/saket/compose/shared/rx/ObservableTake.kt
+++ shared/src/commonMain/kotlin/me/saket/compose/shared/rx/ObservableTake.kt
@@ -1,39 +1,38 @@
 package me.saket.compose.shared.rx
 
+import com.badoo.reaktive.completable.CompletableCallbacks
 import com.badoo.reaktive.disposable.Disposable
+import com.badoo.reaktive.disposable.DisposableWrapper
 import com.badoo.reaktive.observable.Observable
 import com.badoo.reaktive.observable.ObservableObserver
 import com.badoo.reaktive.observable.observable
+import com.badoo.reaktive.utils.atomic.AtomicInt
 
 /**
- * TODO: Remove this once its upstreamed to Reaktive.
  * Emit only the first [limit] items emitted by source.
  */
 fun <T> Observable<T>.take(limit: Int): Observable<T> {
-  require(limit > 0) { "count >= 0 required but it was $limit" }
+  require(limit > 0) { "count > 0 required but it was $limit" }
 
   return observable { emitter ->
-    var remaining = limit
+    val disposableWrapper = DisposableWrapper()
+    emitter.setDisposable(disposableWrapper)
 
-    subscribe(object : ObservableObserver<T> {
-      override fun onComplete() {
-        emitter.onComplete()
-      }
+    val remaining = AtomicInt(limit)
 
-      override fun onError(error: Throwable) {
-        emitter.onError(error)
-      }
-
+    subscribe(object : ObservableObserver<T>, CompletableCallbacks by emitter {
       override fun onNext(value: T) {
-        emitter.onNext(value)
-
-        if (--remaining == 0) {
-          onComplete()
+        if (remaining.value > 0) {
+          val stop = remaining.addAndGet(-1) == 0
+          emitter.onNext(value)
+          if (stop) {
+            onComplete()
+          }
         }
       }
 
       override fun onSubscribe(disposable: Disposable) {
-        emitter.setDisposable(disposable)
+        disposableWrapper.set(disposable)
       }
     })
   }
