103e630854584f129fd384a1575c6df7512b8801
Saket Narayan <saket@saket.me>
2019-11-06 21:19:27 -0500

    Update accent color

--- shared/src/commonMain/kotlin/me/saket/compose/shared/theme/DraculaThemePalette.kt
+++ shared/src/commonMain/kotlin/me/saket/compose/shared/theme/DraculaThemePalette.kt
@@ -2,16 +2,16 @@ package me.saket.compose.shared.theme
 
 val COLOR_BLACK_ROCK = "#2F313F".toColor()
 val COLOR_TUNA = "#353846".toColor()
-val COLOR_SCREAMING_GREEN = "#50FA7B".toColor()
+val COLOR_SPRING_GREEN = "#85F589".toColor()
 val COLOR_GHOST = "#c2c3c7".toColor()
 val COLOR_ANAKIWA = "#8be8fd".toColor()
 
 object DraculaThemePalette : ThemePalette(
     primaryColor = COLOR_BLACK_ROCK,
     primaryColorDark = COLOR_BLACK_ROCK,
-    accentColor = COLOR_SCREAMING_GREEN,
+    accentColor = COLOR_SPRING_GREEN,
     window = WindowTheme(backgroundColor = COLOR_TUNA),
-    textColorHeading = COLOR_SCREAMING_GREEN,
+    textColorHeading = COLOR_SPRING_GREEN,
     textColorSecondary = COLOR_GHOST,
     fabColor = COLOR_ANAKIWA
 )
