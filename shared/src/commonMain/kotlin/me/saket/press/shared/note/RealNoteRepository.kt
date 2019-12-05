fc14d25187b7eaf029a19810c637efebb820b2e5
Saket Narayan <saket@saket.me>
2019-12-04 19:24:43 -0500

    Avoid auto-saving content if its unchanged

--- shared/src/commonMain/kotlin/me/saket/press/shared/note/RealNoteRepository.kt
+++ shared/src/commonMain/kotlin/me/saket/press/shared/note/RealNoteRepository.kt
@@ -3,6 +3,10 @@ package me.saket.press.shared.note
 import com.badoo.reaktive.completable.Completable
 import com.badoo.reaktive.completable.completableFromFunction
 import com.badoo.reaktive.observable.Observable
+import com.badoo.reaktive.observable.asCompletable
+import com.badoo.reaktive.observable.filter
+import com.badoo.reaktive.observable.map
+import com.badoo.reaktive.observable.take
 import com.badoo.reaktive.scheduler.Scheduler
 import com.benasher44.uuid.Uuid
 import me.saket.press.data.shared.Note
@@ -11,6 +15,7 @@ import me.saket.press.shared.db.DateTimeAdapter
 import me.saket.press.shared.rx.asObservable
 import me.saket.press.shared.rx.mapToList
 import me.saket.press.shared.rx.mapToOneOrOptional
+import me.saket.press.shared.rx.mapToSome
 import me.saket.press.shared.time.Clock
 import me.saket.press.shared.util.Optional
 
@@ -54,13 +59,18 @@ internal class RealNoteRepository(
   }
 
   override fun update(noteUuid: Uuid, content: String): Completable {
-    return completableFromFunction {
-      noteQueries.updateContent(
-          uuid = noteUuid,
-          content = content,
-          updatedAt = clock.nowUtc()
-      )
-    }
+    return note(noteUuid)
+        .take(1)
+        .mapToSome()
+        .filter { note -> note.content.trim() != content.trim() }
+        .map {
+          noteQueries.updateContent(
+              uuid = noteUuid,
+              content = content,
+              updatedAt = clock.nowUtc()
+          )
+        }
+        .asCompletable()
   }
 
   override fun markAsDeleted(noteUuid: Uuid): Completable {
