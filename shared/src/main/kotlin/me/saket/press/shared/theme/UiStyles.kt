a595f1a4a8b92be1fd57ae0cc3a83f7c53f7668f
Saket Narayan <saket@saket.me>
2019-12-15 00:42:55 -0500

    Throw an error on debug if the theme font is italic with >400 weight

--- shared/src/main/kotlin/me/saket/press/shared/theme/UiStyles.kt
+++ shared/src/main/kotlin/me/saket/press/shared/theme/UiStyles.kt
@@ -6,6 +6,7 @@ import android.os.Build.VERSION_CODES.P
 import android.text.TextUtils.TruncateAt
 import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
+import me.saket.press.shared.BuildConfig
 import me.saket.press.shared.R
 import me.saket.press.shared.theme.UiStyles.FontVariant.BOLD
 import me.saket.press.shared.theme.UiStyles.FontVariant.ITALIC
@@ -32,6 +33,10 @@ private fun UiStyles.Text.readFont(view: TextView): Typeface {
     val isItalic = font.variant.isItalic
     Typeface.create(fontFamily, font.variant.weight, isItalic)
   } else {
+    if (BuildConfig.DEBUG && font.variant.weight > 400 && font.variant == ITALIC) {
+      throw TODO("Find a way backward-compatible way to render composite styles (i.e., italic + bold)")
+    }
+
     val styleInt = when (font.variant) {
       REGULAR -> Typeface.NORMAL
       ITALIC -> Typeface.ITALIC
