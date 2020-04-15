f65db9280906d231d735d7fd560339bcb26b49b0
Saket Narayan <saket@saket.me>
2020-04-15 01:50:37 -0400

    Add a blank macOS app

---
+++ native/mac/mac/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  mac
+//
+//  Created by Saket Narayan on 4/15/20.
+//  Copyright © 2020 Saket Narayan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello World")
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
