38004add8793efb5466c6194dd513cd5e0dc41ac
Saket Narayan <saket@saket.me>
2020-04-18 18:47:57 -0400

    Add missing macos implementations for running shared tests

---
+++ shared/src/macosTest/kotlin/me/saket/press/shared/CanaryTest.kt
@@ -0,0 +1,11 @@
+package me.saket.press.shared
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class CanaryTest {
+
+  @Test fun canary() {
+    assertEquals(Platform.name, "macOS")
+  }
+}
