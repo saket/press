815eb522fa8583c8d13e0423d1c97042e4db0231
Saket Narayan <saket@saket.me>
2020-04-18 17:59:10 -0400

    Add macos target to wysiwyg module

---
+++ wysiwyg/src/macosMain/kotlin/me/saket/wysiwyg/util/Timber.kt
@@ -0,0 +1,8 @@
+package me.saket.wysiwyg.util
+
+@Suppress("NOTHING_TO_INLINE")
+internal actual object Timber {
+  actual inline fun i(message: String): Unit = TODO()
+  actual inline fun d(message: String): Unit = TODO()
+  actual inline fun w(message: String): Unit = TODO()
+}
