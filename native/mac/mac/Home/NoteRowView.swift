4d95854072b0f923fa7c5c0169b441d495a8343d
Saket Narayan <saket@squareup.com>
2020-04-30 23:31:22 -0400

    mac: Extract note row into its own view

---
+++ native/mac/mac/Home/NoteRowView.swift
@@ -0,0 +1,26 @@
+//
+// Created by Saket Narayan on 4/30/20.
+// Copyright (c) 2020 Saket Narayan. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import shared
+
+struct NoteRowView: View {
+  let note: HomeUiModel.Note
+
+  var body: some View {
+    VStack(alignment: .leading, spacing: 8) {
+      Text(note.title)
+        .lineLimit(1)
+        .truncationMode(.tail)
+        .frame(maxWidth: .infinity, alignment: .leading)
+
+      Text(note.body)
+        .lineLimit(2)
+        .truncationMode(.tail)
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }.padding(8)
+  }
+}
