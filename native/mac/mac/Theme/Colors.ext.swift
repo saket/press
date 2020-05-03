29bfe5c2aff06830d8417b3e437c9ff9ba125b7d
Saket Narayan <saket@squareup.com>
2020-05-02 22:54:27 -0400

    mac: Set window background from theme

--- native/mac/mac/Theme/Colors.ext.swift
+++ native/mac/mac/Theme/Colors.ext.swift
@@ -13,11 +13,7 @@ extension View {
 
 public extension Color {
   init(_ argb: Int32) {
-    let red = (argb >> 16) & 0xFF
-    let green = (argb >> 8) & 0xFF
-    let blue = argb & 0xFF
-    let alpha = (argb >> 24) & 0xFF
-
+    let (red, green, blue, alpha) = readColorChannels(argb)
     self.init(
       red: Double(red) / 255.0,
       green: Double(green) / 255.0,
@@ -25,4 +21,29 @@ public extension Color {
       opacity: Double(alpha) / 255.0
     )
   }
+}
+
+public extension NSColor {
+  convenience init(_ argb: Int32) {
+    let (red, green, blue, alpha) = readColorChannels(argb)
+    let redFloat = CGFloat(red) / 255.0
+    let greenF = CGFloat(green) / 255.0
+    let blueF = CGFloat(blue) / 255.0
+    let alphaF = CGFloat(alpha) / 255.0
+
+    self.init(
+      red: redFloat,
+      green: greenF,
+      blue: blueF,
+      alpha: alphaF
+    )
+  }
+}
+
+private func readColorChannels(_ argb: Int32) -> (Int32, Int32, Int32, Int32) {
+  let red = (argb >> 16) & 0xFF
+  let green = (argb >> 8) & 0xFF
+  let blue = argb & 0xFF
+  let alpha = (argb >> 24) & 0xFF
+  return (red, green, blue, alpha)
 }
