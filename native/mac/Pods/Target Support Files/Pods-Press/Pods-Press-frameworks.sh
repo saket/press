75f3f48d72b63cd30602d5f9e858079ca4671b82
Saket Narayan <saket@squareup.com>
2020-05-15 23:36:08 -0400

    mac: Add HotKey library

--- native/mac/Pods/Target Support Files/Pods-Press/Pods-Press-frameworks.sh
+++ native/mac/Pods/Target Support Files/Pods-Press/Pods-Press-frameworks.sh
@@ -197,9 +197,11 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/HotKey/HotKey.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Swinject/Swinject.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/HotKey/HotKey.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Swinject/Swinject.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
