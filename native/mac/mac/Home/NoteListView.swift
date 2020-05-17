ed09ce3dfe7cbcdc9792b05c5a11f22bc2a2f5ea
Saket Narayan <saket@squareup.com>
2020-05-17 04:52:22 -0400

    mac: Remove list background color only for note list

--- native/mac/mac/Home/NoteListView.swift
+++ native/mac/mac/Home/NoteListView.swift
@@ -8,6 +8,7 @@ import SwiftUI
 import shared
 import Swinject
 import Combine
+import Introspect
 
 struct NoteListView: View {
   @EnvironmentObject var theme: AppTheme
@@ -23,7 +24,9 @@ struct NoteListView: View {
       }
         .removeListMargins()
         .animation(nil) // Avoid animating content (including background) changes.
-    }.animation(.easeIn, value: model)
+    }
+      .removeBackground()
+      .animation(.easeIn, value: model)
   }
 
   init(_ model: HomeUiModel, selection: Binding<NoteId?>) {
@@ -46,28 +49,3 @@ extension Publisher {
   }
 }
 
-// List on macOS uses an opaque background with no option for
-// removing/changing it. listRowBackground() doesn't work either.
-// This workaround works because List is backed by NSTableView
-// on macOS.
-extension NSTableView {
-  open override func viewDidMoveToWindow() {
-    super.viewDidMoveToWindow()
-
-    backgroundColor = .clear
-    enclosingScrollView?.drawsBackground = false
-
-    /// SwiftUI doesn't offer any way to set highlight
-    /// colors so we draw them manually instead.
-    selectionHighlightStyle = .none
-  }
-}
-
-extension View {
-  /// SwiftUI has additional spacing around list items on macOS
-  /// with option for removing them. Using negative margins until
-  /// this is fixed.
-  func removeListMargins() -> some View {
-    return padding(.horizontal, -8).padding(.vertical, -4)
-  }
-}
--- native/mac/mac/Util/Views.ext.swift
+++ native/mac/mac/Util/Views.ext.swift
@@ -5,6 +5,7 @@
 
 import Foundation
 import AppKit
+import SwiftUI
 
 extension NSTextView {
   func setPaddings(horizontal: CGFloat, vertical: CGFloat) {
@@ -12,5 +13,31 @@ extension NSTextView {
     /// on the padding and focus the first/last letter in the line.
     textContainer!.lineFragmentPadding = horizontal
     textContainerInset = NSSize(width: 0, height: vertical)
+  }
+}
+
+extension List {
+  /// List on macOS uses an opaque background with no option for
+  /// removing/changing it. listRowBackground() doesn't work either.
+  /// This workaround works because List is backed by NSTableView
+  /// on macOS.
+  func removeBackground() -> some View {
+    return introspectTableView { tableView in
+      tableView.backgroundColor = .clear
+      tableView.enclosingScrollView!.drawsBackground = false
+
+      /// SwiftUI doesn't offer any way to set highlight
+      /// colors so we draw them manually instead.
+      tableView.selectionHighlightStyle = .none
+    }
+  }
+}
+
+extension View {
+  /// SwiftUI has additional spacing around list items on macOS
+  /// with option for removing them. Using negative margins until
+  /// this is fixed.
+  func removeListMargins() -> some View {
+    return padding(.horizontal, -8).padding(.vertical, -4)
   }
 }
