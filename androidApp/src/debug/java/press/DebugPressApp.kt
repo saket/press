a6d4580cbd30b70ae942acd45151fac457289e7e
Saket Narayan <saket@squareup.com>
2020-06-30 18:00:16 -0400

    Revert "Disable Flipper to make Robolectric work"
    This reverts commit 1a28c28df44e78603f0122b28220c4cb35745079.

--- androidApp/src/debug/java/press/DebugPressApp.kt
+++ androidApp/src/debug/java/press/DebugPressApp.kt
@@ -1,5 +1,12 @@
 package press
 
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin
+import com.facebook.soloader.SoLoader
 import press.di.AppComponent
 import press.di.DaggerAppComponent
 import timber.log.Timber
@@ -12,13 +19,13 @@ class DebugPressApp : PressApp() {
     super.onCreate()
     Timber.plant(DebugTree())
 
-//    SoLoader.init(this, false)
-//    if (FlipperUtils.shouldEnableFlipper(this)) {
-//      val client = AndroidFlipperClient.getInstance(this)
-//      client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
-//      client.addPlugin(DatabasesFlipperPlugin(this))
-//      client.start()
-//    }
+    SoLoader.init(this, false)
+    if (FlipperUtils.shouldEnableFlipper(this)) {
+      val client = AndroidFlipperClient.getInstance(this)
+      client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+      client.addPlugin(DatabasesFlipperPlugin(this))
+      client.start()
+    }
   }
 
   override fun buildDependencyGraph(): AppComponent =
