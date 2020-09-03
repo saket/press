3d1505ca23baaf95b92b8fb79e32ddf0e8c68000
Saket Narayan <saket@squareup.com>
2020-09-03 00:09:27 -0400

    android: Use androidx.lifecycle for detecting when app is foregrounded

--- androidApp/src/main/java/press/PressApp.kt
+++ androidApp/src/main/java/press/PressApp.kt
@@ -1,18 +1,19 @@
 package press
 
-import android.app.Activity
 import android.app.Application
-import android.os.Bundle
 import android.os.Looper
+import androidx.lifecycle.Lifecycle.Event.ON_START
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.ProcessLifecycleOwner
 import androidx.work.WorkManager
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.android.schedulers.AndroidSchedulers
 import me.saket.press.shared.di.SharedComponent
 import press.di.AppComponent
-import press.home.HomeActivity
 import press.sync.BackgroundSyncWorker
 
-abstract class PressApp : Application() {
+abstract class PressApp : Application(), LifecycleObserver {
   companion object {
     lateinit var component: AppComponent
   }
@@ -28,32 +29,12 @@ abstract class PressApp : Application() {
     RxAndroidPlugins.setInitMainThreadSchedulerHandler {
       AndroidSchedulers.from(Looper.getMainLooper(), true)
     }
-
-    BackgroundSyncWorker.schedule(WorkManager.getInstance(this))
-    doOnAppResume {
-      component.syncCoordinator().trigger()
-    }
+    ProcessLifecycleOwner.get().lifecycle.addObserver(this)
   }
 
-  private fun doOnAppResume(action: () -> Unit) {
-    registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
-      var resumedActivities = 0
-
-      override fun onActivityResumed(activity: Activity) {
-        if (resumedActivities++ == 0) {
-          action()
-        }
-      }
-
-      override fun onActivityPaused(activity: Activity) {
-        resumedActivities--
-      }
-
-      override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) = Unit
-      override fun onActivityStarted(activity: Activity) = Unit
-      override fun onActivityStopped(activity: Activity) = Unit
-      override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
-      override fun onActivityDestroyed(activity: Activity) = Unit
-    })
+  @OnLifecycleEvent(ON_START)
+  fun onAppForegrounded() {
+    BackgroundSyncWorker.schedule(WorkManager.getInstance(this))
+    component.syncCoordinator().trigger()
   }
 }
