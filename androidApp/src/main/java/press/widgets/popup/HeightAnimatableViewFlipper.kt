26492e102eb23da5104dc500655430631f43b699
Saket Narayan <saket@squareup.com>
2020-10-05 10:23:02 -0400

    Cascade: format HeightAnimatableViewFlipper

--- androidApp/src/main/java/press/widgets/popup/HeightAnimatableViewFlipper.kt
+++ androidApp/src/main/java/press/widgets/popup/HeightAnimatableViewFlipper.kt
@@ -8,11 +8,8 @@ import android.graphics.Rect
 import android.graphics.drawable.Drawable
 import android.view.MotionEvent
 import android.view.View
-import android.view.ViewGroup.LayoutParams
 import android.view.ViewOutlineProvider
-import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.AnimationUtils
-import android.view.animation.DecelerateInterpolator
 import android.widget.FrameLayout.LayoutParams.MATCH_PARENT
 import android.widget.FrameLayout.LayoutParams.WRAP_CONTENT
 import android.widget.ViewFlipper
@@ -21,32 +18,24 @@ import androidx.core.view.doOnLayout
 import androidx.interpolator.view.animation.FastOutSlowInInterpolator
 import me.saket.press.R
 
-/** A [ViewFlipper] that smoothly changes its height for the currently displayed child. */
+/**
+ * A [ViewFlipper] that smoothly changes its height for the currently displayed child.
+ */
 class HeightAnimatableViewFlipper(context: Context) : BaseHeightClippableFlipper(context) {
-  fun goForward(child: View) {
-    show(child, forward = true)
-  }
 
-  fun goBack(child: View) {
-    show(child, forward = false)
-  }
-
-  private fun show(view: View, forward: Boolean) {
-    val index = if (forward) childCount else 0
-    val params = view.layoutParams ?: LayoutParams(MATCH_PARENT, WRAP_CONTENT)
-
-    if (childCount == 0) {
+  fun show(view: View, forward: Boolean) {
+    enqueueAnimation {
+      val index = if (forward) childCount else 0
+      val params = view.layoutParams ?: LayoutParams(MATCH_PARENT, WRAP_CONTENT)
       super.addView(view, index, params)
-      return
-    }
 
-    // Queue children so that they show up one-by-one.
-    waitForOngoingAnimation {
-      super.addView(view, index, params)
+      if (childCount == 1) {
+        return@enqueueAnimation
+      }
 
       setupFlipAnimation(forward)
-      val prevView = getChildAt(displayedChild)
-      displayedChild = indexOfChild(view)
+      val prevView = displayedChildView!!
+      displayedChildView = view
 
       doOnLayout {
         animateHeight(
@@ -64,18 +53,18 @@ class HeightAnimatableViewFlipper(context: Context) : BaseHeightClippableFlipper
     }
   }
 
-  override fun addView(child: View, index: Int, params: android.view.ViewGroup.LayoutParams) {
-    throw error("Use show() / goForward() / goBack() instead")
-  }
+  fun goForward(child: View) =
+    show(child, forward = true)
 
-  private fun waitForOngoingAnimation(action: () -> Unit) {
+  fun goBack(child: View) =
+    show(child, forward = false)
+
+  override fun addView(child: View, index: Int, params: android.view.ViewGroup.LayoutParams) =
+    throw error("Use show() / goForward() / goBack() instead")
+
+  private fun enqueueAnimation(action: () -> Unit) {
     if (!animator.isRunning) action()
     else animator.doOnEnd { action() }
-  }
-
-  override fun onDetachedFromWindow() {
-    animator.cancel()
-    super.onDetachedFromWindow()
   }
 
   private fun setupFlipAnimation(goingForward: Boolean) {
@@ -109,11 +98,6 @@ abstract class BaseHeightClippableFlipper(context: Context) : ViewFlipper(contex
     outlineProvider = ViewOutlineProvider.BACKGROUND
   }
 
-  override fun onDetachedFromWindow() {
-    animator.cancel()
-    super.onDetachedFromWindow()
-  }
-
   protected fun animateHeight(from: Int, to: Int, onEnd: () -> Unit) {
     animator.cancel()
     animator = ObjectAnimator.ofFloat(0f, 1f).apply {
@@ -128,6 +112,19 @@ abstract class BaseHeightClippableFlipper(context: Context) : ViewFlipper(contex
       doOnEnd { onEnd() }
       start()
     }
+  }
+
+  private fun setClippedHeight(newHeight: Int) {
+    clipBounds2 = (clipBounds2 ?: Rect()).also {
+      it.set(left, top, right, newHeight)
+      clipBounds = it
+    }
+    invalidate()
+  }
+
+  override fun onDetachedFromWindow() {
+    animator.cancel()
+    super.onDetachedFromWindow()
   }
 
   @Suppress("DEPRECATION")
@@ -146,14 +143,6 @@ abstract class BaseHeightClippableFlipper(context: Context) : ViewFlipper(contex
     (background as DrawSuppressibleDrawable?).withDrawSuppressed {
       super.draw(canvas)
     }
-  }
-
-  private fun setClippedHeight(newHeight: Int) {
-    clipBounds2 = (clipBounds2 ?: Rect()).also {
-      it.set(left, top, right, newHeight)
-      clipBounds = it
-    }
-    invalidate()
   }
 
   override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
