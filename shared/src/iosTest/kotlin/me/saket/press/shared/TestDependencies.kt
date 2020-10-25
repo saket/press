a7ebea307f5d89c5828c82e11be126dc81189bb1
Saket Narayan <saket@squareup.com>
2020-10-25 01:10:14 -0400

    Add common impl for runBlocking{}

--- shared/src/iosTest/kotlin/me/saket/press/shared/TestDependencies.kt
+++ shared/src/iosTest/kotlin/me/saket/press/shared/TestDependencies.kt
@@ -1,5 +1,12 @@
 package me.saket.press.shared
 
+import kotlinx.coroutines.CoroutineScope
 import me.saket.press.shared.sync.git.DeviceInfo
 
 actual fun testDeviceInfo(): DeviceInfo = TODO()
+
+actual fun <T> runBlocking(block: suspend CoroutineScope.() -> T): T {
+  return kotlinx.coroutines.runBlocking {
+    block(this)
+  }
+}
