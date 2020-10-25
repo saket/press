7b8f381aae11892c22e1b8b03a75971a151c61ec
Saket Narayan <saket@squareup.com>
2020-10-25 01:10:14 -0400

    Add common impl for runBlocking{}

--- shared/src/commonTest/kotlin/me/saket/press/shared/TestDependencies.kt
+++ shared/src/commonTest/kotlin/me/saket/press/shared/TestDependencies.kt
@@ -1,5 +1,8 @@
 package me.saket.press.shared
 
+import kotlinx.coroutines.CoroutineScope
 import me.saket.press.shared.sync.git.DeviceInfo
 
 expect fun testDeviceInfo(): DeviceInfo
+
+expect fun <T> runBlocking(block: suspend CoroutineScope.() -> T): T
--- shared/src/test/kotlin/me/saket/press/shared/TestDependencies.kt
+++ shared/src/test/kotlin/me/saket/press/shared/TestDependencies.kt
@@ -1,5 +1,6 @@
 package me.saket.press.shared
 
+import kotlinx.coroutines.CoroutineScope
 import me.saket.press.shared.sync.git.DeviceInfo
 import me.saket.press.shared.sync.git.File
 import java.nio.file.Files
@@ -7,4 +8,10 @@ import java.nio.file.Files
 actual fun testDeviceInfo() = object : DeviceInfo {
   override val appStorage get() = File(Files.createTempDirectory("press_").toString())
   override fun deviceName() = "Test"
+}
+
+actual fun <T> runBlocking(block: suspend CoroutineScope.() -> T): T {
+  return kotlinx.coroutines.runBlocking {
+    block(this)
+  }
 }
