b13567d5aede03c09834ee9401112e1c89a5f9b3
Saket Narayan <saket@squareup.com>
2020-10-28 02:57:03 -0400

    Implement testDeviceInfo() for macOS

--- shared/src/macosTest/kotlin/me/saket/press/shared/TestDependencies.kt
+++ shared/src/macosTest/kotlin/me/saket/press/shared/TestDependencies.kt
@@ -1,9 +1,15 @@
 package me.saket.press.shared
 
+import kotlinx.cinterop.toKString
 import kotlinx.coroutines.CoroutineScope
 import me.saket.press.shared.sync.git.DeviceInfo
+import me.saket.press.shared.sync.git.File
+import platform.posix.getenv
 
-actual fun testDeviceInfo(): DeviceInfo = TODO()
+actual fun testDeviceInfo() = object : DeviceInfo {
+  override val appStorage: File get() = File(getenv("TMPDIR")!!.toKString())
+  override fun deviceName() = "Test"
+}
 
 actual fun <T> runBlocking(block: suspend CoroutineScope.() -> T): T {
   return kotlinx.coroutines.runBlocking {
