39704e9db47303a235ae76d9105834d085d6d438
Saket Narayan <saket@squareup.com>
2020-10-28 16:19:41 -0400

    Move runBlocking to Platform.kt

--- shared/src/commonTest/kotlin/me/saket/press/shared/TestDependencies.kt
+++ shared/src/commonTest/kotlin/me/saket/press/shared/TestDependencies.kt
@@ -1,8 +1,5 @@
 package me.saket.press.shared
 
-import kotlinx.coroutines.CoroutineScope
 import me.saket.press.shared.sync.git.DeviceInfo
 
 expect fun testDeviceInfo(): DeviceInfo
-
-expect fun <T> runBlocking(block: suspend CoroutineScope.() -> T): T
--- shared/src/macosTest/kotlin/me/saket/press/shared/TestDependencies.kt
+++ shared/src/macosTest/kotlin/me/saket/press/shared/TestDependencies.kt
@@ -2,7 +2,6 @@ package me.saket.press.shared
 
 import com.soywiz.klock.DateTime
 import kotlinx.cinterop.toKString
-import kotlinx.coroutines.CoroutineScope
 import me.saket.press.shared.sync.git.DeviceInfo
 import me.saket.press.shared.sync.git.File
 import me.saket.wysiwyg.atomicLazy
@@ -11,14 +10,8 @@ import platform.posix.getenv
 actual fun testDeviceInfo() = object : DeviceInfo {
   override val appStorage: File by atomicLazy {
     val tempPath = getenv("TMPDIR")!!.toKString()
-    File(tempPath + "press_${DateTime.nowUnixLong()}").makeDirectory()
+    File(tempPath + "press_${DateTime.nowUnixLong()}/").makeDirectories()
   }
 
   override fun deviceName() = "Test"
-}
-
-actual fun <T> runBlocking(block: suspend CoroutineScope.() -> T): T {
-  return kotlinx.coroutines.runBlocking {
-    block(this)
-  }
 }
