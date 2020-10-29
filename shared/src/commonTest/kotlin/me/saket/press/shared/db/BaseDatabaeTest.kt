5e3a390086334b2f479d1f72f4ee3281f03ae7e3
Saket Narayan <saket@squareup.com>
2020-10-28 23:12:49 -0400

    Switch to korio library for files IO

--- shared/src/commonTest/kotlin/me/saket/press/shared/db/BaseDatabaeTest.kt
+++ shared/src/commonTest/kotlin/me/saket/press/shared/db/BaseDatabaeTest.kt
@@ -10,6 +10,7 @@ expect fun inMemorySqlDriver(): SqlDriver
 /**
  * Creates an in-memory database and closes it before and after each test.
  * This class exists because JUnit rules aren't a thing (yet) in Kotlin tests.
+ *
  * The name of this class is not a typo.
  */
 abstract class BaseDatabaeTest {
