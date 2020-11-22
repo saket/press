84667aee79047112106f1a093b19aba6dea440b1
Saket Narayan <saket@squareup.com>
2020-11-22 04:28:05 -0500

    android: Ignore pull-to-collapse when started from toolbar

--- androidApp/src/main/java/press/extensions/InboxRecyclerView.ext.kt
+++ androidApp/src/main/java/press/extensions/InboxRecyclerView.ext.kt
@@ -7,16 +7,18 @@ import me.saket.inboxrecyclerview.page.InterceptResult.INTERCEPTED
 import me.saket.inboxrecyclerview.page.OnPullToCollapseInterceptor
 import me.saket.inboxrecyclerview.page.SimplePageStateChangeCallbacks
 
-fun interceptPullToCollapseOnView(view: View): OnPullToCollapseInterceptor {
+fun interceptPullToCollapseOnView(scrollableView: View, toolbar: View? = null): OnPullToCollapseInterceptor {
   return { downX, downY, upwardPull ->
-    val touchLiesOnView = view.locationOnScreen().contains(downX.toInt(), downY.toInt())
-
-    if (touchLiesOnView) {
-      val directionInt = if (upwardPull) +1 else -1
-      val canScrollFurther = view.canScrollVertically(directionInt)
-      if (canScrollFurther) INTERCEPTED else IGNORED
-    } else {
+    if (toolbar?.locationOnScreen()?.contains(downX.toInt(), downY.toInt()) == true) {
       IGNORED
+    } else {
+      if (scrollableView.locationOnScreen().contains(downX.toInt(), downY.toInt())) {
+        val directionInt = if (upwardPull) +1 else -1
+        val canScrollFurther = scrollableView.canScrollVertically(directionInt)
+        if (canScrollFurther) INTERCEPTED else IGNORED
+      } else {
+        IGNORED
+      }
     }
   }
 }
