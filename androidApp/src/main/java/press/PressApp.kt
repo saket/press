e2045411182b44722d58daaef75ce48584d8d26a
Saket Narayan <saket@squareup.com>
2020-12-21 04:03:34 -0500

    android: Create launcher shortcut dynamically

--- androidApp/src/main/java/press/PressApp.kt
+++ androidApp/src/main/java/press/PressApp.kt
@@ -1,7 +1,12 @@
 package press
 
 import android.app.Application
+import android.content.Intent
+import android.content.pm.ShortcutInfo
+import android.content.pm.ShortcutManager
+import android.graphics.drawable.Icon
 import android.os.Looper
+import androidx.core.content.getSystemService
 import androidx.lifecycle.Lifecycle.Event.ON_START
 import androidx.lifecycle.Lifecycle.Event.ON_STOP
 import androidx.lifecycle.LifecycleObserver
@@ -10,8 +15,10 @@ import androidx.lifecycle.ProcessLifecycleOwner
 import androidx.work.WorkManager
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.android.schedulers.AndroidSchedulers
+import me.saket.press.R
 import me.saket.press.shared.di.SharedComponent
 import press.di.AppComponent
+import press.navigation.TheActivity
 import press.sync.BackgroundSyncWorker
 
 abstract class PressApp : Application(), LifecycleObserver {
@@ -32,6 +39,22 @@ abstract class PressApp : Application(), LifecycleObserver {
 
     BackgroundSyncWorker.schedule(WorkManager.getInstance(this))
     ProcessLifecycleOwner.get().lifecycle.addObserver(this)
+
+    createLauncherShortcuts()
+  }
+
+  /**
+   * Press uses dynamic shortcuts instead of static shortcuts so that
+   * [TheActivity] can be opened in "singleTask" launch mode.
+   */
+  private fun createLauncherShortcuts() {
+    val newNote = ShortcutInfo.Builder(this, "new_note")
+      .setShortLabel(getString(R.string.shortcut_new_note_label))
+      .setIcon(Icon.createWithResource(this, R.drawable.ic_note_add_black_24dp))
+      .setIntent(Intent(this, TheActivity::class.java).setAction(Intent.ACTION_SEND))
+      .build()
+
+    getSystemService<ShortcutManager>()!!.dynamicShortcuts = listOf(newNote)
   }
 
   @OnLifecycleEvent(ON_START)
