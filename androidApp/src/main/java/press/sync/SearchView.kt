e6a03b6a09db953046bc3c7e4ac465ed7b5e0829
Saket Narayan <saket@squareup.com>
2020-12-23 02:44:12 -0500

    android: Introduce MaterialTextInputLayout

--- androidApp/src/main/java/press/sync/SearchView.kt
+++ androidApp/src/main/java/press/sync/SearchView.kt
@@ -3,7 +3,7 @@ package press.sync
 import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.Color.BLACK
-import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.EditorInfo.IME_FLAG_NO_FULLSCREEN
 import android.widget.EditText
 import androidx.core.view.updatePadding
 import com.google.android.material.textfield.TextInputLayout
@@ -13,34 +13,20 @@ import me.saket.press.shared.theme.applyStyle
 import me.saket.press.shared.theme.blendWith
 import press.extensions.textColor
 import press.theme.themeAware
+import press.widgets.MaterialTextInputLayout
 import press.widgets.dp
 
-class SearchView(context: Context) : TextInputLayout(context) {
+class SearchView(context: Context) : MaterialTextInputLayout(context) {
   init {
-    addView(
-      EditText(context).apply {
-        id = R.id.search_view_field
-        background = null
-        imeOptions = imeOptions or EditorInfo.IME_FLAG_NO_FULLSCREEN
-        applyStyle(smallBody)
-        themeAware {
-          textColor = it.textColorPrimary
-        }
-        updatePadding(top = dp(24), bottom = dp(8), left = dp(16), right = dp(16))
-      }
-    )
-
+    editText.id = R.id.search_view_field
+    editText.applyStyle(smallBody)
     endIconMode = END_ICON_CLEAR_TEXT
-    importantForAutofill = IMPORTANT_FOR_AUTOFILL_NO_EXCLUDE_DESCENDANTS
 
-    boxBackgroundMode = BOX_BACKGROUND_FILLED
     themeAware {
-      hintTextColor = ColorStateList.valueOf(it.accentColor)
-      boxBackgroundColor = it.window.backgroundColor.blendWith(BLACK, ratio = 0.1f)
-      boxStrokeColor = it.accentColor
+      editText.textColor = it.textColorPrimary
 
       // Note to self: this is not the same as hintTextColor
-      setEndIconTintList(ColorStateList.valueOf(editText!!.currentHintTextColor))
+      setEndIconTintList(ColorStateList.valueOf(editText.currentHintTextColor))
     }
   }
 }
---
+++ androidApp/src/main/java/press/widgets/MaterialTextInputLayout.kt
@@ -0,0 +1,40 @@
+package press.widgets
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import androidx.core.view.updatePadding
+import com.google.android.material.textfield.TextInputLayout
+import me.saket.press.shared.theme.TextStyles
+import me.saket.press.shared.theme.applyStyle
+import me.saket.press.shared.theme.blendWith
+import press.theme.themeAware
+
+@Suppress("LeakingThis")
+open class MaterialTextInputLayout(context: Context) : TextInputLayout(context) {
+  init {
+    addView(
+      EditText(context).apply {
+        background = null
+        imeOptions = imeOptions or EditorInfo.IME_FLAG_NO_FULLSCREEN
+        updatePadding(top = dp(24), bottom = dp(8), left = dp(16), right = dp(16))
+      }
+    )
+
+    importantForAutofill = IMPORTANT_FOR_AUTOFILL_NO_EXCLUDE_DESCENDANTS
+    boxBackgroundMode = BOX_BACKGROUND_FILLED
+
+    themeAware {
+      hintTextColor = ColorStateList.valueOf(it.accentColor)
+      boxBackgroundColor = it.window.backgroundColor.blendWith(Color.BLACK, ratio = 0.1f)
+      boxStrokeColor = it.accentColor
+      setHelperTextColor(ColorStateList.valueOf(it.textColorHint))
+    }
+  }
+
+  override fun getEditText(): EditText {
+    return super.getEditText()!!
+  }
+}
