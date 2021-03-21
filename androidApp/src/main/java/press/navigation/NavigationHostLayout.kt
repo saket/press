aee26a9f43e6a13a19dc4e931dff1d694975ae09
Saket Narayan <saket@squareup.com>
2021-03-20 22:35:10 -0400

    android: Synchronize keyboard animation with the app's content

--- androidApp/src/main/java/press/navigation/NavigationHostLayout.kt
+++ androidApp/src/main/java/press/navigation/NavigationHostLayout.kt
@@ -1,13 +1,29 @@
 package press.navigation
 
-import android.content.Context
+import android.app.Activity
 import android.view.MotionEvent
 import android.widget.FrameLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import press.widgets.insets.KeyboardInsetsChangeAnimator
 
-class NavigationHostLayout(context: Context) : FrameLayout(context) {
+class NavigationHostLayout(activity: Activity) : FrameLayout(activity) {
+  init {
+    // Press is going to handle insets on its own from this point.
+    WindowCompat.setDecorFitsSystemWindows(activity.window, false)
+
+    val insetAnimator = KeyboardInsetsChangeAnimator(this)
+    ViewCompat.setWindowInsetsAnimationCallback(this, insetAnimator)
+    ViewCompat.setOnApplyWindowInsetsListener(this, insetAnimator)
+  }
+
   override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
     // Don't leak touch events to background screens, which
     // can happen if the foreground View doesn't consume an event.
-    return getChildAt(childCount - 1)?.dispatchTouchEvent(ev) ?: super.dispatchTouchEvent(ev)
+    getChildAt(childCount - 1)?.let { foreground ->
+      ev.offsetLocation(-foreground.left.toFloat(), -foreground.top.toFloat())
+      return foreground.dispatchTouchEvent(ev)
+    }
+    return super.dispatchTouchEvent(ev)
   }
 }
---
+++ androidApp/src/main/java/press/widgets/insets/KeyboardInsetsChangeAnimator.kt
@@ -0,0 +1,66 @@
+package press.widgets.insets
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.OnApplyWindowInsetsListener
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsAnimationCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsCompat.Type.ime
+import androidx.core.view.WindowInsetsCompat.Type.systemBars
+
+/**
+ * Synchronizes keyboard entry & exit animation with the app's layout.
+ *
+ * Google's [official sample](https://github.com/android/user-interface-samples) prefers to move each UI component
+ * individually by animating their translationY for maximum efficiency. Press takes a shortcut by updating the padding
+ * for the entire app on every frame of the animation. This isn't ideal, but it makes the whole logic extremely simple
+ * to maintain instead of doing it in every screen.
+ */
+class KeyboardInsetsChangeAnimator(
+  private val layout: ViewGroup
+) : WindowInsetsAnimationCompat.Callback(DISPATCH_MODE_CONTINUE_ON_SUBTREE),
+  OnApplyWindowInsetsListener {
+
+  private lateinit var lastWindowInsets: WindowInsetsCompat
+  private var isKeyboardAnimating = false
+
+  override fun onPrepare(animation: WindowInsetsAnimationCompat) {
+    if (animation.typeMask and ime() != 0) {
+      isKeyboardAnimating = true
+    }
+  }
+
+  override fun onApplyWindowInsets(view: View, insets: WindowInsetsCompat): WindowInsetsCompat {
+    lastWindowInsets = insets
+
+    // When the keyboard isn't animating, the insets are applied immediately.
+    // Otherwise, they're applied during each frame of the animation in onProgress().
+    if (!isKeyboardAnimating) {
+      layout.setPadding(insets)
+    }
+
+    // Stop the insets being dispatched any further into the view hierarchy.
+    return WindowInsetsCompat.CONSUMED
+  }
+
+  override fun onProgress(
+    insets: WindowInsetsCompat,
+    runningAnimations: List<WindowInsetsAnimationCompat>
+  ): WindowInsetsCompat {
+    layout.setPadding(insets)
+    return insets
+  }
+
+  override fun onEnd(animation: WindowInsetsAnimationCompat) {
+    if (isKeyboardAnimating && (animation.typeMask and ime() != 0)) {
+      isKeyboardAnimating = false
+    }
+  }
+
+  private fun View.setPadding(windowInsets: WindowInsetsCompat) {
+    windowInsets.getInsets(systemBars() or ime()).let {
+      setPadding(it.left, it.top, it.right, it.bottom)
+    }
+  }
+}
