894294eb088d75bda3cb832dead95ae788604a00
Saket Narayan <saket@squareup.com>
2021-04-07 17:55:10 -0400

    android: Subscribe to theme changes immediately if View is alrady attached

--- androidApp/src/main/java/press/theme/themeAware.kt
+++ androidApp/src/main/java/press/theme/themeAware.kt
@@ -5,9 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.jakewharton.rxbinding3.view.attaches
 import com.jakewharton.rxbinding3.view.detaches
 import io.reactivex.Observable
+import io.reactivex.Single
+import me.saket.press.shared.listenRx
 import me.saket.press.shared.theme.AppTheme
 import me.saket.press.shared.theme.ThemePalette
-import me.saket.press.shared.listenRx
 import press.PressApp
 import press.extensions.onDestroys
 
@@ -18,7 +19,15 @@ fun themePalette(): Observable<ThemePalette> =
 
 fun View.themeAware(onThemeChange: (ThemePalette) -> Unit) {
   val stream = themePalette()
-  attaches()
+
+  val attaches = attaches().mergeWith(Single.create {
+    // Because RxBinding doesn't emit anything if the View is already attached.
+    if (isAttachedToWindow) {
+      it.onSuccess(Unit)
+    }
+  })
+
+  attaches
     .switchMap { stream }
     .takeUntil(detaches())
     .mergeWith(stream.take(1))  // Don't wait till attach for the first emission.
