277996241547dcfe982a8dfd4a83a94cc54aac46
Saket Narayan <saket@squareup.com>
2021-04-24 16:43:52 -0400

    Bump dependencies and kick out jcenter

--- shared/src/commonMain/kotlin/me/saket/press/shared/di/koinComponent.kt
+++ shared/src/commonMain/kotlin/me/saket/press/shared/di/koinComponent.kt
@@ -1,14 +1,15 @@
 package me.saket.press.shared.di
 
-import org.koin.core.context.KoinContextHandler
 import org.koin.core.parameter.ParametersDefinition
 import org.koin.core.qualifier.Qualifier
+import org.koin.mp.KoinPlatformTools
 
-// This exists because making components extend KoinComponent results in
-// Dagger complaining about not being able to resolve koin related classes.
+// This exists because making components extend KoinComponent prevents Dagger
+// (in :androidApp) from being able to resolve koin classes that is not in its classpath.
 internal inline fun <reified T> koin(
   qualifier: Qualifier? = null,
   noinline parameters: ParametersDefinition? = null
 ): T {
-  return KoinContextHandler.get().get(parameters = parameters, qualifier = qualifier)
+  val koin = KoinPlatformTools.defaultContext().get()
+  return koin.get(parameters = parameters, qualifier = qualifier)
 }
